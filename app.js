f851ab637264f6e16cc9205901a3a79c715dd03a
Алексей Акулич <61252310+loki87by@users.noreply.github.com>
2021-03-24 22:43:03 +0300

    refactor: remove extra blank lines

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var addButton=document.getElementsByTagName("button")[0];
 var incompleteTaskHolder=document.getElementById("incomplete");
 var completedTasksHolder=document.getElementById("completed");
 
-
 //New task list item
 var createNewTaskElement=function(taskString){
   var listItem=document.createElement("li");
@@ -30,7 +29,6 @@ var createNewTaskElement=function(taskString){
   deleteButtonImg.className="main__delete-img";
   deleteButton.appendChild(deleteButtonImg);
 
-
   //and appending.
   listItem.appendChild(checkBox);
   listItem.appendChild(label);
@@ -40,35 +38,25 @@ var createNewTaskElement=function(taskString){
   return listItem;
 }
 
-
-
 var addTask=function(){
   console.log("Add Task...");
   if (!taskInput.value) return;
   var listItem=createNewTaskElement(taskInput.value);
-
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskCompleted);
-
   taskInput.value="";
-
 }
 
 //Edit an existing task.
-
 var editTask=function(){
   console.log("Edit Task...");
   console.log("Change 'edit' to 'save'");
-
-
   var listItem=this.parentNode;
-
   var editInput=listItem.querySelector(".main__task-input");
   var label=listItem.querySelector("label");
   var editBtn=listItem.querySelector(".main__edit-btn");
   var containsClass=listItem.classList.contains("main__edit-mode");
   if(containsClass){
-
     label.classList.add("main__task-label-edit");
     label.innerText=editInput.value;
     editBtn.innerText="Edit";
@@ -79,32 +67,24 @@ var editTask=function(){
     editInput.value=label.innerText;
     editBtn.innerText="Save";
   }
-
   listItem.classList.toggle("main__edit-mode");
 };
-
 
 //Delete task.
 var deleteTask=function(){
   console.log("Delete Task...");
-
   var listItem=this.parentNode;
   var ul=listItem.parentNode;
   ul.removeChild(listItem);
-
 }
-
 
 //Mark task completed
 var taskCompleted=function(){
   console.log("Complete Task...");
-
   var listItem=this.parentNode;
   completedTasksHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskIncomplete);
-
 }
-
 
 var taskIncomplete=function(){
   console.log("Incomplete Task...");
@@ -113,26 +93,19 @@ var taskIncomplete=function(){
   bindTaskEvents(listItem,taskCompleted);
 }
 
-
-
 var ajaxRequest=function(){
   console.log("AJAX Request");
 }
 
-
-
 addButton.onclick=addTask;
 addButton.addEventListener("click",addTask);
 addButton.addEventListener("click",ajaxRequest);
-
 
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
   console.log("bind list item events");
   var checkBox=taskListItem.querySelector(".main__task-checkbox");
   var editButton=taskListItem.querySelector(".main__edit-btn");
   var deleteButton=taskListItem.querySelector(".main__delete-btn");
-
-
   editButton.onclick=editTask;
   deleteButton.onclick=deleteTask;
   checkBox.onchange=checkBoxEventHandler;
@@ -140,23 +113,14 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
 
 //cycle over incompleteTaskHolder ul list items
 for (var i=0; i<incompleteTaskHolder.children.length;i++){
-
   bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
 }
-
-
-
 
 //cycle over completedTasksHolder ul list items
 for (var i=0; i<completedTasksHolder.children.length;i++){
   bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
 }
 
-
-
-
 // Issues with usability don't get seen until they are in front of a human tester.
-
-//prevent creation of empty tasks.
-
-//Change edit to save when you are in edit mode.
+// prevent creation of empty tasks.
+// Change edit to save when you are in edit mode.
